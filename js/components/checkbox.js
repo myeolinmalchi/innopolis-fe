9dd032c62a2abd829c8b72d68569e8c778c0178a
myeolinmalchi <rkd2274@pusan.ac.kr>
2022-11-16 18:01:49 +0900

    DELETE footer.js

--- js/components/checkbox.js
+++ js/components/checkbox.js
@@ -1,14 +1,18 @@
 customElements.define(
     'checkbox-default',
     class extends HTMLElement {
-        wrapper;
         checkbox;
+        label;
         constructor() {
             super();
-            this.wrapper = document.createElement('label');
 
             this.checkbox = document.createElement('input');
             this.checkbox.setAttribute('type', 'checkbox');
+            this.checkbox.setAttribute('id', 'checkbox');
+
+            this.label = document.createElement('label');
+            this.label.setAttribute('id', 'label');
+            this.label.setAttribute('for', 'checkbox');
         }
         connectedCallback() {
             const shadow = this.attachShadow({ mode: 'open' });
@@ -17,24 +21,47 @@ customElements.define(
 
             const stylesheet = document.createElement('style');
             stylesheet.innerText = `
-                input {
-                    width: ${size === 's' ? '13px' : '18px'};
-                    height: ${size === 's' ? '13px' : '18px'};
-                    border: 1px solid var(--stroke-gray-03);
+                [type=checkbox] {
+                    display: none;
+                }
+
+                [type=checkbox] + label {
+                    box-sizing: border-box;
+                    display: inline-block;
+                    width: 16px;
+                    height: 16px;
+                    border: 1px solid var(--color-gray-filled);
                     border-radius: 1px;
                 }
 
-                input:checked:hover {
+                [type=checkbox]:checked + label {
+                    border: none;
+                    background: var(--color-blue-filled);
                 }
 
-                input:hover {
-
+                [type=checkbox]:checked + label:hover {
+                    border: none;
+                    background: var(--color-blue-hover);
                 }
 
-                input:checked {
-
+                [type=checkbox] + label:hover {
+                    border: 1px solid var(--color-gray-hover);
                 }
             `;
+
+            this.label.onclick = () => {
+                this.checkbox.setAttribute('checked', !this.checkbox.checked);
+            };
+
+            const link1 = document.createElement('link');
+            link1.setAttribute('href', '../css/common.css');
+            link1.setAttribute('rel', 'stylesheet');
+
+            const link2 = document.createElement('link');
+            link2.setAttribute('href', '../css/reset.css');
+            link2.setAttribute('rel', 'stylesheet');
+
+            shadow.append(link1, link2, stylesheet, this.checkbox, this.label);
         }
     },
 );

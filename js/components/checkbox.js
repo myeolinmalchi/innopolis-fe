7b429d44c75c68f5dfd89649ea862f77b0a75762
myeolinmalchi <rkd2274@pusan.ac.kr>
2022-11-28 15:08:00 +0900

    임시

--- js/components/checkbox.js
+++ js/components/checkbox.js
@@ -1,46 +1,37 @@
 customElements.define(
     'checkbox-default',
-    class extends HTMLElement{
-        constructor(){ 
+    class extends HTMLElement {
+        constructor() {
             super();
         }
-        
-        connectedCallback(){
-            this.attachShadow({mode : 'open'});
+
+        connectedCallback() {
+            this.attachShadow({ mode: 'open' });
             const wrapper = document.createElement('div');
             const label = document.createElement('label');
             const checkbox = document.createElement('input');
             const textbox = document.createElement('span');
-            checkbox.setAttribute('type','checkbox');
+            checkbox.setAttribute('type', 'checkbox');
 
-            
             const check = this.getAttribute('check');
             const text = this.getAttribute('text');
-            const state =this.getAttribute('state');
+            const state = this.getAttribute('state');
             const size = this.getAttribute('size');
-            const style = document.createElement('style');;
-            
-            const [height, width, font_size] = (function (){
-                switch(size){
+            const style = document.createElement('style');
+
+            const [height, width, font_size] = (function () {
+                switch (size) {
                     case 'M':
-                        return [
-                            13,
-                            13,
-                            12
-                        ];
+                        return [13, 13, 12];
                     case 'L':
-                        return [
-                            16.67,
-                            16.67,
-                            16
-                        ];
+                        return [16.67, 16.67, 16];
                 }
             })();
-            if(check === 'true'){
+            if (check === 'true') {
                 checkbox.checked = true;
             }
-            if(state === 'disabled'){
-                checkbox.disabled= true;
+            if (state === 'disabled') {
+                checkbox.disabled = true;
             }
             style.textContent = `
                 .size{
@@ -50,18 +41,16 @@ customElements.define(
                 .font{
                     font-size : ${font_size};                    
                 }
-                `
+                `;
 
             textbox.innerText = text;
-            label.setAttribute('class','font');
-            checkbox.setAttribute('class','size');
+            label.setAttribute('class', 'font');
+            checkbox.setAttribute('class', 'size');
             this.shadowRoot.append(style);
             this.shadowRoot.appendChild(wrapper);
             wrapper.appendChild(label);
             label.appendChild(checkbox);
             label.appendChild(textbox);
         }
-    
-
-    }
-)
+    },
+);

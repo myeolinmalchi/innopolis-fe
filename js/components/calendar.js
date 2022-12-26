f5231de234c9380fde2aa95a10ef96d0c19d7a8b
myeolinmalchi <rkd2274@pusan.ac.kr>
2022-12-26 09:47:11 +0900

    update

--- js/components/calendar.js
+++ js/components/calendar.js
@@ -2,21 +2,24 @@ customElements.define(
     'calendar-unit',
     class extends HTMLElement {
         state;
+        content;
+
         typeStyleMap = new Map([
             [
                 'none',
                 () => {
-                    this.style.borderRadius = 'none';
-                    this.style.background = 'var(--background-white-01)';
+                    this.content.style.borderRadius = 'none';
+                    this.content.style.background =
+                        'var(--background-white-01)';
                     switch (this.state) {
                         case 'default':
-                            this.style.color = 'var(--text-primary)';
+                            this.content.style.color = 'var(--text-primary)';
                             break;
                         case 'disabled':
-                            this.style.color = 'var(--text-disabled)';
+                            this.content.style.color = 'var(--text-disabled)';
                             break;
                         case 'accent':
-                            this.style.color = 'var(--text-red)';
+                            this.content.style.color = 'var(--text-red)';
                             break;
                     }
                 },
@@ -24,57 +27,80 @@ customElements.define(
             [
                 'one',
                 () => {
-                    this.style.background = 'var(--color-blue-filled)';
-                    this.style.color = 'var(--text-white)';
-                    this.style.borderRadius = '42px';
+                    this.content.style.background = 'var(--color-blue-filled)';
+                    this.content.style.color = 'var(--text-white)';
+                    this.content.style.borderRadius = '42px';
                 },
             ],
             [
                 'left',
                 () => {
-                    this.style.background = 'var(--color-blue-filled)';
-                    this.style.color = 'var(--text-white)';
-                    this.style.borderRadius = '42px 0px 0px 42px';
+                    this.content.style.background = 'var(--color-blue-filled)';
+                    this.content.style.color = 'var(--text-white)';
+                    this.content.style.borderRadius = '42px 0px 0px 42px';
                 },
             ],
             [
                 'right',
                 () => {
-                    this.style.background = 'var(--color-blue-filled)';
-                    this.style.color = 'var(--text-white)';
-                    this.style.borderRadius = '0px 42px 42px 0px';
+                    this.content.style.background = 'var(--color-blue-filled)';
+                    this.content.style.color = 'var(--text-white)';
+                    this.content.style.borderRadius = '0px 42px 42px 0px';
                 },
             ],
             [
                 'center',
                 () => {
-                    this.style.background = 'var(--color-blue-filled)';
-                    this.style.color = 'var(--text-white)';
-                    this.style.borderRadius = '0px';
+                    this.content.style.background = 'var(--color-blue-filled)';
+                    this.content.style.color = 'var(--text-white)';
+                    this.content.style.borderRadius = '0px';
                 },
             ],
         ]);
+        constructor() {
+            super();
+            this.content = document.createElement('span');
+        }
         connectedCallback() {
-            const [state, type] = [
+            const [state, type, mode] = [
                 this.getAttribute('state') ?? 'default',
                 this.getAttribute('type') ?? 'none',
+                this.getAttribute('mode') ?? 'normal',
             ];
 
+            this.style = `
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                width: 14%;
+            `;
+
             this.state = state;
-            this.setCommonStyle();
+            this.setCommonStyle(mode);
             this.typeStyleMap.get(type)();
+            this.append(this.content);
         }
 
-        setCommonStyle() {
-            this.classList += 'body-03-bd';
-            this.style.cursor = 'pointer';
-            this.style.width = '42px';
-            this.style.height = '42px';
+        setCommonStyle(mode) {
+            this.style.height = mode === 'normal' ? '95px' : '51px';
             this.style.boxSizing = 'border-box';
             this.style.display = 'flex';
             this.style.alignItems = 'center';
             this.style.justifyContent = 'center';
             this.style.margin = '0px';
+
+            this.content.classList += 'body-03-bd';
+            this.style.textAlign = 'center';
+            this.content.style = `
+                cursor: pointer;
+                width: 32px;
+                height: 32px;
+                text-align: center;
+                margin: auto;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+            `;
         }
     },
 );
@@ -85,10 +111,12 @@ customElements.define(
         connectedCallback() {
             this.style.display = 'flex';
             this.style.flexDirection = 'column';
-            this.style.padding = '32px 20px';
-            this.style.width = '340px';
+            this.style.padding = '64px 20px';
+            this.style.width = '100%';
             this.style.margin = '0px';
             this.style.boxSizing = 'border-box';
+            this.style.marginTop = '16px';
+            this.style.borderBottom = '1px solid var(--stroke-gray-01)';
         }
     },
 );
@@ -107,7 +135,8 @@ customElements.define(
             this.date = new Date();
             this.style.display = 'flex';
             this.style.flexWrap = 'wrap';
-            this.style.width = '294px';
+            this.style.width = '100%';
+            this.style.justifyContent = 'center';
             setTimeout(() => {
                 this.period = this.nextElementSibling;
             });
@@ -134,6 +163,7 @@ customElements.define(
         }
 
         render() {
+            const dates = [1, 2, 13, 14, 15, 20, 21, 22, 26, 27, 29];
             this.innerHTML = '';
             const year = this.date.getFullYear();
             const month = this.date.getMonth();
@@ -176,8 +206,8 @@ customElements.define(
                         }
                     }
                 }
-                unit.innerHTML = i;
-                unit.onclick = () => {
+                unit.content.innerHTML = i;
+                /*unit.onclick = () => {
                     if (!this.start_day) {
                         this.start_day = date;
                         this.period?.st_date.setStartday(date);
@@ -196,7 +226,7 @@ customElements.define(
                         }
                     }
                     this.render();
-                };
+                };*/
                 this.units.push(unit);
             }
 
@@ -229,27 +259,26 @@ customElements.define(
                         }
                     }
                 }
-                unit.innerHTML = i;
-                unit.onclick = () => {
+                unit.content.innerHTML = i;
+                if (dates.includes(i)) {
+                    unit.setAttribute('type', 'one');
+                }
+                /*unit.onclick = () => {
                     if (!this.start_day) {
                         this.start_day = date;
-                        this.period?.st_date.setStartday(date);
                     } else if (!this.end_day) {
                         if (this.start_day.getTime() === date.getTime()) {
                             this.start_day = undefined;
-                            this.period?.st_date.setStartday(undefined);
                         } else if (date > this.start_day) {
                             this.end_day = date;
-                            this.period?.ed_date.setStartday(date);
                         }
                     } else {
                         if (this.end_day.getTime() === date.getTime()) {
                             this.end_day = undefined;
-                            this.period?.ed_date.setStartday(undefined);
                         }
                     }
                     this.render();
-                };
+                };*/
                 this.units.push(unit);
             }
 
@@ -259,7 +288,7 @@ customElements.define(
                 const date = new Date(year, month + 1, i);
                 unit.setAttribute('state', 'disabled');
                 unit.setAttribute('type', 'none');
-                unit.innerHTML = i;
+                unit.content.innerHTML = i;
                 if (this.start_day) {
                     if (this.end_day) {
                         if (this.start_day < date && this.end_day > date) {
@@ -279,7 +308,7 @@ customElements.define(
                         }
                     }
                 }
-                unit.onclick = () => {
+                /*unit.onclick = () => {
                     if (!this.start_day) {
                         this.start_day = date;
                         this.period?.st_date.setStartday(date);
@@ -298,7 +327,7 @@ customElements.define(
                         }
                     }
                     this.render();
-                };
+                };*/
                 this.units.push(unit);
             }
 
@@ -324,29 +353,36 @@ customElements.define(
         units;
         container;
         unit_container;
+        top_container;
         constructor() {
             super();
             this.container = document.createElement('div');
             this.container.style.display = 'flex';
             this.container.style.flexDirection = 'row';
             this.container.style.alignItems = 'center';
-            this.container.style.justifyContent = 'space-between';
+            this.container.style.justifyContent = 'center';
             this.container.style.width = '100%';
-            this.container.style.marginBottom = '32px';
+            this.container.style.marginBottom = '72px';
+            this.container.style.gap = '28px';
 
             this.unit_container = document.createElement('div');
             this.unit_container.style.display = 'flex';
             this.unit_container.style.width = '100%';
+            this.unit_container.style.justifyContent = 'center';
 
-            this.unit_container.innerHTML = `
-                <calendar-unit state="accent">일</calendar-unit>
-                <calendar-unit>월</calendar-unit>
-                <calendar-unit>화</calendar-unit>
-                <calendar-unit>수</calendar-unit>
-                <calendar-unit>목</calendar-unit>
-                <calendar-unit>금</calendar-unit>
-                <calendar-unit>토</calendar-unit>
-            `;
+            ['일', '월', '화', '수', '목', '금', '토']
+                .map((date) => {
+                    const unit = document.createElement('calendar-unit');
+                    unit.content.innerHTML = `${date}`;
+                    if (date === '일') {
+                        unit.setAttribute('state', 'accent');
+                    }
+                    unit.setAttribute('mode', 'short');
+                    return unit;
+                })
+                .forEach((unit) => {
+                    this.unit_container.appendChild(unit);
+                });
         }
         connectedCallback() {
             setTimeout(() => {
@@ -357,7 +393,7 @@ customElements.define(
                 this.style.alignItems = 'center';
                 this.style.justifyContent = 'space-between';
                 this.style.flexWrap = 'wrap';
-                this.style.width = '294px';
+                this.style.width = '100%';
                 this.render();
             });
         }
@@ -373,27 +409,28 @@ customElements.define(
 <path d="M6.875 11L1.875 6L6.875 1" stroke="#4B5563" stroke-width="1.875" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>`;
             leftArrow.style.order = '0';
-            leftArrow.onclick = () => {
+            /*leftArrow.onclick = () => {
                 this.units.prevMonth();
                 const year = this.units.date.getFullYear();
                 const month = this.units.date.getMonth() + 1;
                 center.innerText = `${year}.${month}`;
-            };
+            };*/
             const rightArrow = document.createElement('span');
             rightArrow.innerHTML = `<svg width="8" height="12" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M1.125 11L6.125 6L1.125 1" stroke="#4B5563" stroke-width="1.875" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>`;
             rightArrow.style.order = '2';
-            rightArrow.onclick = () => {
+            /*rightArrow.onclick = () => {
                 this.units.nextMonth();
                 const year = this.units.date.getFullYear();
                 const month = this.units.date.getMonth() + 1;
                 center.innerText = `${year}.${month}`;
-            };
+            };*/
 
             const center = document.createElement('span');
             center.innerText = `${year}.${month}`;
             center.style.order = '1';
+            center.style.fontSize = '36px';
 
             this.container.append(leftArrow, center, rightArrow);
             this.append(this.container, this.unit_container);

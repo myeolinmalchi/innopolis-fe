a3656c0dd8891538d49d69ebd77b58115bc6985b
MINO <whyknow0604@naver.com>
2023-01-18 21:29:36 +0900

    복사 방지 기능 필요(문자 및 그림 블록지정금지)

--- js/components/footer.js
+++ js/components/footer.js
@@ -385,3 +385,9 @@ customElements.define('footer-default', footer_default);
     },
 );
 
+window.addEventListener("copy", (e) => {
+    alert("본 데이터는 연구목적으로만 활용이 가능하며, 무단 전재 및 재배포를 금지합니다. 저작권에 위배되는 행위 시 사전 경고 없이 형사 고발 조치됨을 알려드립니다.");
+    e.preventDefault();
+    e.clipboardData.clearData("Text"); // 클립보드에 저장된 컨텐츠 삭제
+  });
+
